01856a7bd9ae6f9518bf31a9da8268799b6a4957
InezMallandain <inezvmallandain@gmail.com>
2022-01-11 09:40:41 +0200

    add Add to do Functionality
    A user can not add a todo list item by typing in item title and hitting enter on the keyboard

--- js/app.js
+++ js/app.js
@@ -1,23 +1,34 @@
 const clear = document.querySelector(".clear");
 const dateElement = document.getElementById("date");
 const list = document.getElementById("list");
-const input = document.getElementById("input");
+const input = document.getElementById("item");
 
 const CHECK = "fa-check-circle";
 const UNCHECK = "fa-cicrcle-thin";
 const LINE_THROUGH = "linethrough";
 
-const options={weekday :"long", month : "short", day : "numeric"}
-const today = new date ();
-dateElement.innerHTML= today.tolocaledatestring ("en-US", options);
+const options = { weekday: "long", month: "short", day: "numeric" };
+const today = new Date();
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);
 
-function addtodo(todo) {
-    const item =`
+function addToDo(todo) {
+  const item = `<li class="item">
     <i class="fa fa-circle-thin-co" job="complete" id="0"></i>
       <p class="text">${todo}</p>
-      <i= class="fa fa-trash-o de" job="delete" id="0"></i>
-    
+      <i class="fa fa-trash-o de" job="delete" id="0"></i>
+    </li>
     `;
-    const position="beforeend";
-    list.insertAdjacentElement(position, item)
+  // Create an HTML Object with
+  var htmlObject = document.createElement("div");
+  htmlObject.innerHTML = item;
+  const position = "beforeend";
+  list.insertAdjacentElement(position, htmlObject);
 }
+document.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    const toDo = input.value;
+    if (toDo) {
+      addToDo(toDo);
+    }
+  }
+});
