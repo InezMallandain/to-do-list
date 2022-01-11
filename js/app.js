4ac72facce0a627030b035cadd51decd34f046c1
InezMallandain <inezvmallandain@gmail.com>
2022-01-11 09:01:36 +0200

    add css and fonts

--- js/app.js
+++ js/app.js
@@ -10,3 +10,14 @@ const LINE_THROUGH = "linethrough";
 const options={weekday :"long", month : "short", day : "numeric"}
 const today = new date ();
 dateElement.innerHTML= today.tolocaledatestring ("en-US", options);
+
+function addtodo(todo) {
+    const item =`
+    <i class="fa fa-circle-thin-co" job="complete" id="0"></i>
+      <p class="text">${todo}</p>
+      <i= class="fa fa-trash-o de" job="delete" id="0"></i>
+    
+    `;
+    const position="beforeend";
+    list.insertAdjacentElement(position, item)
+}
